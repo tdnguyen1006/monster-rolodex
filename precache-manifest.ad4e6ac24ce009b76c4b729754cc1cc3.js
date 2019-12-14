6bc92bf53ddaf787c4d5b5b819820202884702ab
Toan Nguyen <tdnguyen1006@gmail.com>
2019-12-13 22:27:11 -0500

    Updates

---
+++ precache-manifest.ad4e6ac24ce009b76c4b729754cc1cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f4805e5884b70c29484b0a2abe3eed",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "619cc39dd98d8a67525f",
+    "url": "/monster-rolodex/static/css/main.3555a378.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "619cc39dd98d8a67525f",
+    "url": "/monster-rolodex/static/js/main.82768e8f.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
